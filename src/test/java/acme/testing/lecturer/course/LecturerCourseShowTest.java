072a1f57d887204bc98a2320a31906f5cf71d0a6
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-05-08 11:24:36 +0200

    In progress

---
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseShowTest.java
@@ -0,0 +1,99 @@
+/*
+ * EmployerApplicationShowTest.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.lecturer.course;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.courses.Course;
+import acme.testing.TestHarness;
+
+public class LecturerCourseShowTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected LecturerCourseTestRepository repository;
+
+	// Test methods -----------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int courseIndex, final String code, final String title, final String abstractCourse, final String retailPrice, final String link, final String draftMode) {
+		// HINT: this test signs in as an employer, lists his or her jobs, selects one of them and checks that it's as expected.
+
+		super.signIn("lecturer1", "lecturer1");
+
+		super.clickOnMenu("Lecturer", "List of courses");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(courseIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstractCourse", abstractCourse);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("link", link);
+		super.checkInputBoxHasValue("draftMode", draftMode);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test200Negative() {
+		// HINT: there's no negative test case for this listing, since it doesn't
+		// HINT+ involve filling in any forms.
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to show a duty of a job that is in draft mode or
+		// HINT+ not available, but wasn't published by the principal;
+
+		Collection<Course> courses;
+		String param;
+
+		super.signIn("lecturer1", "lecturer1");
+		courses = this.repository.findManyCoursesByLecturerUsername("lecturer2");
+		for (final Course course : courses)
+			if (course.isDraftMode()) {
+				param = String.format("id=%d", course.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/lecturer/course/show", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/lecturer/course/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("employer2", "employer2");
+				super.request("/lecturer/course/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("worker1", "worker1");
+				super.request("/lecturer/course/show", param);
+				super.checkPanicExists();
+				super.signOut();
+			}
+	}
+
+}
