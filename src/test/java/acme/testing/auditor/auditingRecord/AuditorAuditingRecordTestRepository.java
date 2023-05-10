6f1cd8e52f04c9f848f528d3150ea0b9f745e827
paomarsan <paomarsan@alum.us.es>
2023-05-11 01:21:30 +0200

    Task 156: Test show auditingRecord DONE

--- src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordTestRepository.java
+++ src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordTestRepository.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.springframework.data.jpa.repository.Query;
 
+import acme.entities.auditingRecords.AuditingRecord;
 import acme.entities.audits.Audit;
 import acme.framework.repositories.AbstractRepository;
 
@@ -12,5 +13,8 @@ public interface AuditorAuditingRecordTestRepository extends AbstractRepository 
 
 	@Query("select a from Audit a where a.auditor.userAccount.username = :username")
 	Collection<Audit> findAuditsByAuditorUsername(String username);
+
+	@Query("select ar from AuditingRecord ar where ar.audit.id=:id")
+	Collection<AuditingRecord> findAuditingRecordsByAuditId(int id);
 
 }
