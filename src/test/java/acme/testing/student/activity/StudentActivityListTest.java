5cf147324c79b9270775904cc10f2106209a52f4
abernalmar <abernalmar@gmail.com>
2023-05-10 13:49:09 +0200

    Task 141: Test de list de student enrolment hecho

---
+++ src/test/java/acme/testing/student/activity/StudentActivityListTest.java
@@ -0,0 +1,71 @@
+
+package acme.testing.student.activity;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.enrolments.Enrolment;
+import acme.testing.TestHarness;
+import acme.testing.student.enrolment.StudentEnrolmentTestRepository;
+
+public class StudentActivityListTest extends TestHarness {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected StudentEnrolmentTestRepository repository;
+
+
+	// Test methods -----------------------------------------------------------
+	@ParameterizedTest
+	@CsvFileSource(resources = "/student/activity/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int enrolmentIndex, final String code, final int activityIndex, final String title, final String activityType) {
+		// HINT: this test authenticates as an employer, then lists his or her jobs, 
+		// HINT+ selects one of them, and check that it has the expected duties.
+
+		super.signIn("student1", "student1");
+
+		super.clickOnMenu("Student", "List of enrolments");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(enrolmentIndex, 0, code);
+		super.clickOnListingRecord(enrolmentIndex);
+		super.checkInputBoxHasValue("code", code);
+		super.clickOnButton("See workbook");
+		super.clickOnButton("Activities");
+
+		super.checkListingExists();
+		super.checkColumnHasValue(activityIndex, 0, title);
+		super.checkColumnHasValue(activityIndex, 1, activityType);
+		super.clickOnListingRecord(activityIndex);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test200Negative() {
+		// HINT: there's no negative test case for this listing, since it doesn't
+		// HINT+ involve filling in any forms.
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		Collection<Enrolment> enrolments;
+		String param;
+
+		enrolments = this.repository.findManyEnrolmentsByStudentUsername("student1");
+		for (final Enrolment enrolment : enrolments)
+			if (enrolment.isDraftMode()) {
+				param = String.format("id=%d", enrolment.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/student/enrolment/list", param);
+				super.checkPanicExists();
+			}
+	}
+}
---
+++ src/test/java/acme/testing/student/activity/StudentActivityTestRepository.java
@@ -0,0 +1,18 @@
+
+package acme.testing.student.activity;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.entities.enrolments.Enrolment;
+import acme.framework.repositories.AbstractRepository;
+
+public interface StudentActivityTestRepository extends AbstractRepository {
+
+	@Query("select e from Enrolment e where e.student.userAccount.username = :username")
+	Collection<Enrolment> findManyEnrolmentsByStudentUsername(String username);
+
+	//	@Query("select a from Activity a where a.student.userAccount.username = :username")
+	//	Collection<Activity> findManyActivitiesByStudentUsername(String username);
+}
