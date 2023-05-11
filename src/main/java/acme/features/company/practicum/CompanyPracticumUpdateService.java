98c35a356e655d0b3119543209c86c6b98ae3738
meriglmar <meriglmar@alum.us.es>
2023-05-11 16:23:57 +0200

    Task 151: Validación estimatedTotalTime funciona

--- src/main/java/acme/features/company/practicum/CompanyPracticumUpdateService.java
+++ src/main/java/acme/features/company/practicum/CompanyPracticumUpdateService.java
@@ -75,7 +75,7 @@ public class CompanyPracticumUpdateService extends AbstractService<Company, Prac
 		courseId = super.getRequest().getData("course", int.class);
 		course = this.practicumRepository.findCourseById(courseId);
 
-		super.bind(object, "code", "title", "abstract$", "goals");
+		super.bind(object, "code", "title", "abstract$", "goals", "estimatedTotalTime");
 		object.setCourse(course);
 	}
 
@@ -124,7 +124,7 @@ public class CompanyPracticumUpdateService extends AbstractService<Company, Prac
 		courses = this.practicumRepository.findPublishedCourses();
 		choices = SelectChoices.from(courses, "code", object.getCourse());
 
-		tuple = super.unbind(object, "code", "title", "abstract$", "goals");
+		tuple = super.unbind(object, "code", "title", "abstract$", "goals", "estimatedTotalTime");
 		tuple.put("course", choices.getSelected().getKey());
 		tuple.put("courses", choices);
 
