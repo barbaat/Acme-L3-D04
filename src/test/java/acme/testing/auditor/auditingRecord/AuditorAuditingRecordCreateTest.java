9913c95bc77e27d9c63e56c672171a319ca15968
paomarsan <paomarsan@alum.us.es>
2023-05-11 17:10:43 +0200

    Task 156: Test create auditingRecord

--- src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordCreateTest.java
+++ src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordCreateTest.java
@@ -20,7 +20,87 @@ public class AuditorAuditingRecordCreateTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/auditor/auditingRecord/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo,
+	public void test100Positive(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo) {
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkInputBoxHasValue("code", code);
+
+		super.clickOnButton("Auditing-Records");
+		super.clickOnButton("Create");
+
+		super.fillInputBoxIn("subject", subject);
+		super.fillInputBoxIn("assessment", assessment);
+		super.fillInputBoxIn("startTime", startTime);
+		super.fillInputBoxIn("finishTime", finishTime);
+		super.fillInputBoxIn("mark", mark);
+		super.fillInputBoxIn("moreInfo", moreInfo);
+
+		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkInputBoxHasValue("code", code);
+
+		super.clickOnButton("Auditing-Records");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(auditingRecordIndex, 0, subject);
+
+		super.clickOnListingRecord(auditingRecordIndex);
+
+		super.checkInputBoxHasValue("subject", subject);
+		super.checkInputBoxHasValue("assessment", assessment);
+		super.checkInputBoxHasValue("startTime", startTime);
+		super.checkInputBoxHasValue("finishTime", finishTime);
+		super.checkInputBoxHasValue("mark", mark);
+		super.checkInputBoxHasValue("moreInfo", moreInfo);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/auditingRecord/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo) {
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkInputBoxHasValue("code", code);
+
+		super.clickOnButton("Auditing-Records");
+		super.clickOnButton("Create");
+
+		super.fillInputBoxIn("subject", subject);
+		super.fillInputBoxIn("assessment", assessment);
+		super.fillInputBoxIn("startTime", startTime);
+		super.fillInputBoxIn("finishTime", finishTime);
+		super.fillInputBoxIn("mark", mark);
+		super.fillInputBoxIn("moreInfo", moreInfo);
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/auditingRecord/create-positive-exceptional.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100PositiveExceptional(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo,
 		final String confirmation) {
 		super.signIn("auditor1", "auditor1");
 
@@ -72,8 +152,8 @@ public class AuditorAuditingRecordCreateTest extends TestHarness {
 	}
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/auditor/auditingRecord/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo) {
+	@CsvFileSource(resources = "/auditor/auditingRecord/create-negative-exceptional.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200NegativeExceptional(final int auditIndex, final String code, final int auditingRecordIndex, final String subject, final String assessment, final String startTime, final String finishTime, final String mark, final String moreInfo) {
 
 		super.signIn("auditor1", "auditor1");
 
