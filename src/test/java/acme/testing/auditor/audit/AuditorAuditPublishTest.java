cadb4ed3125e7d2218bfad9c72cfb63f41cd9f2c
paomarsan <paomarsan@alum.us.es>
2023-05-12 16:44:11 +0200

    Task 156: Test publish audit DONE

---
+++ src/test/java/acme/testing/auditor/audit/AuditorAuditPublishTest.java
@@ -0,0 +1,159 @@
+
+package acme.testing.auditor.audit;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.audits.Audit;
+import acme.testing.TestHarness;
+
+public class AuditorAuditPublishTest extends TestHarness {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuditorAuditTestRepository repository;
+
+	// Test methods -----------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/audit/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int auditIndex, final String course, final String code, final String conclusion, final String strongPoints, final String weakPoints) {
+		//Publicar correctamente un audit
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("conclusion", conclusion);
+		super.checkInputBoxHasValue("strongPoints", strongPoints);
+		super.checkInputBoxHasValue("weakPoints", weakPoints);
+
+		final Audit audit = this.repository.findAuditByCode(code);
+		final String param = String.format("id=%d", audit.getId());
+		super.checkSubmitExists("Publish");
+		super.clickOnSubmit("Publish");
+		super.checkNotErrorsExist();
+
+		super.request("/auditor/audit/publish", param);
+		super.checkPanicExists();
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("conclusion", conclusion);
+		super.checkInputBoxHasValue("strongPoints", strongPoints);
+		super.checkInputBoxHasValue("weakPoints", weakPoints);
+
+		super.checkNotSubmitExists("Publish");
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/audit/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int auditIndex, final String course, final String code, final String conclusion, final String strongPoints, final String weakPoints) {
+		//No poder publicar un audit porque ya está publicado
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List of audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditIndex, 0, code);
+		super.clickOnListingRecord(auditIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("conclusion", conclusion);
+		super.checkInputBoxHasValue("strongPoints", strongPoints);
+		super.checkInputBoxHasValue("weakPoints", weakPoints);
+
+		final Audit audit = this.repository.findAuditByCode(code);
+		final String param = String.format("id=%d", audit.getId());
+		super.checkNotSubmitExists("Publish");
+
+		super.request("/auditor/audit/publish", param);
+		super.checkPanicExists();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// Publicar audit con otro rol que no es Auditor
+
+		Collection<Audit> audits;
+		String params;
+
+		audits = this.repository.findAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits)
+			if (!audit.isDraftMode()) {
+				params = String.format("id=%d", audit.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/auditor/audit/publish", params);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/auditor/audit/publish", params);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("student2", "student2");
+				super.request("/auditor/audit/register");
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("lecturer1", "lecturer1");
+				super.request("/auditor/audit/register");
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("company1", "company1");
+				super.request("/auditor/audit/register");
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("assistant1", "assistant1");
+				super.request("/auditor/audit/register");
+				super.checkPanicExists();
+				super.signOut();
+			}
+	}
+
+	@Test
+	public void test301Hacking() {
+		//Publicar un audit siendo Auditor pero no el que lo creó
+		Collection<Audit> audits;
+		String params;
+
+		super.signIn("auditor2", "auditor2");
+		audits = this.repository.findAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits) {
+			params = String.format("id=%d", audit.getId());
+			super.request("/auditor/audit/publish", params);
+		}
+		super.signOut();
+	}
+
+}
