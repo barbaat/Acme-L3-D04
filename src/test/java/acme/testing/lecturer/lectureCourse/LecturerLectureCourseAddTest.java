54f828ced9eba35ed2b2d3edea8a509223ee89b8
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-05-12 12:11:28 +0200

    Task 136: In progress

--- src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseAddTest.java
+++ src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseAddTest.java
@@ -1,62 +1,155 @@
 
 package acme.testing.lecturer.lectureCourse;
 
+import java.util.Collection;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.lectures.Lecture;
 import acme.testing.TestHarness;
 
 public class LecturerLectureCourseAddTest extends TestHarness {
 
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected LecturerLectureCourseTestRepository repository;
+
+	// Test data --------------------------------------------------------------
+
+
 	@ParameterizedTest
 	@CsvFileSource(resources = "/lecturer/lectureCourse/add-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int lectureIndex, final String title, final String abstractLecture, final String body, final String estimatedLearningTimeInHours, final String lectureType, final String link, final String draftMode) {
+	public void test100Positive(final int courseIndex, final int lectureIndex, final int lectureCourseIndex, final String code, final String title) {
+
+		// Añadir lecture a un curso que no está publicado
 
 		super.signIn("lecturer1", "lecturer1");
-		super.clickOnMenu("Lecturer", "Create lecture");
+		super.clickOnMenu("Lecturer", "List of courses");
 
-		super.fillInputBoxIn("title", title);
-		super.fillInputBoxIn("abstractLecture", abstractLecture);
-		super.fillInputBoxIn("body", body);
-		super.fillInputBoxIn("estimatedLearningTimeInHours", estimatedLearningTimeInHours);
-		super.fillInputBoxIn("lectureType", lectureType);
-		super.fillInputBoxIn("link", link);
-		super.clickOnSubmit("Create lecture");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(courseIndex, 0, code);
+		super.clickOnListingRecord(courseIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkSubmitExists("Publish a course");
+		super.clickOnButton("Lectures of course");
+
+		super.checkListingEmpty();
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(lectureIndex, 0, title);
+		super.clickOnListingRecord(lectureIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.clickOnButton("Add to course");
+
+		super.checkFormExists();
+		super.fillInputBoxIn("course", code);
+		super.clickOnSubmit("Add");
+
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(courseIndex, 0, code);
+		super.clickOnListingRecord(courseIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkSubmitExists("Publish a course");
+		super.clickOnButton("Lectures of course");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(lectureCourseIndex, 0, title);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lectureCourse/add-negative-published.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200NegativePublishedCourse(final int courseIndex, final int lectureIndex, final int lectureCourseIndex, final String code, final String titleLectureCourse, final String titleLecture) {
+
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(courseIndex, 0, code);
+		super.clickOnListingRecord(courseIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkNotSubmitExists("Publish a course");
+		super.clickOnButton("Lectures of course");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(lectureCourseIndex, 0, titleLectureCourse);
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(lectureIndex, 0, titleLecture);
+		super.clickOnListingRecord(lectureIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", titleLecture);
+		super.clickOnButton("Add to course");
+
+		super.checkFormExists();
+		super.fillInputBoxIn("course", code);
+		super.clickOnSubmit("Add");
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lectureCourse/add-negative-already.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200NegativeAlreadyInCourse(final int courseIndex, final int lectureIndex, final int lectureCourseIndex, final String code, final String title) {
+
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(courseIndex, 0, code);
+		super.clickOnListingRecord(courseIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkSubmitExists("Publish a course");
+		super.clickOnButton("Lectures of course");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(lectureCourseIndex, 0, title);
 
 		super.clickOnMenu("Lecturer", "List of lectures");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
 		super.checkColumnHasValue(lectureIndex, 0, title);
-
 		super.clickOnListingRecord(lectureIndex);
+
+		super.checkFormExists();
 		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("abstractLecture", abstractLecture);
-		super.checkInputBoxHasValue("body", body);
-		super.checkInputBoxHasValue("estimatedLearningTimeInHours", estimatedLearningTimeInHours);
-		super.checkInputBoxHasValue("lectureType", lectureType);
-		super.checkInputBoxHasValue("link", link);
-		super.checkInputBoxHasValue("draftMode", draftMode);
+		super.clickOnButton("Add to course");
 
-		super.signOut();
-	}
-
-	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/lecture/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int lectureIndex, final String title, final String abstractLecture, final String body, final String estimatedLearningTimeInHours, final String lectureType, final String link) {
-
-		super.signIn("lecturer1", "lecturer1");
-		super.clickOnMenu("Lecturer", "Create lecture");
-
-		super.fillInputBoxIn("title", title);
-		super.fillInputBoxIn("abstractLecture", abstractLecture);
-		super.fillInputBoxIn("body", body);
-		super.fillInputBoxIn("estimatedLearningTimeInHours", estimatedLearningTimeInHours);
-		super.fillInputBoxIn("lectureType", lectureType);
-		super.fillInputBoxIn("link", link);
-		super.clickOnSubmit("Create lecture");
-
+		super.checkFormExists();
+		super.fillInputBoxIn("course", code);
+		super.clickOnSubmit("Add");
 		super.checkErrorsExist();
 
 		super.signOut();
@@ -65,15 +158,86 @@ public class LecturerLectureCourseAddTest extends TestHarness {
 	@Test
 	public void test300Hacking() {
 
-		super.checkLinkExists("Sign in");
-		super.request("/lecturer/lecture/create");
-		super.checkPanicExists();
+		// No pueda añadir una lecture a un curso alguien que no tenga el rol de lecturer
 
-		super.signIn("administrator", "administrator");
-		super.request("/lecturer/lecture/create");
-		super.checkPanicExists();
-		super.signOut();
+		Collection<Lecture> lectures;
+		String param;
+		lectures = this.repository.findManyLecturesByLecturerUsername("user-account-lecturer1");
+		for (final Lecture lecture : lectures) {
+			param = String.format("lectureId=%d", lecture.getId());
+			super.checkLinkExists("Sign in");
+			super.request("/lecturer/lecture-course/create" + param);
+			super.checkPanicExists();
 
+			super.signIn("administrator", "administrator");
+			super.request("/lecturer/lecture-course/create" + param);
+			super.checkPanicExists();
+			super.signOut();
+		}
 	}
 
+	@Test
+	public void test301Hacking() {
+
+		/*
+		 * // final Collection<Course> cursos;
+		 * //
+		 * // cursos = this.repository.findManyCoursesByLecturerUsername("user-account-lecturer1");
+		 * // for (final Course curso : cursos) {
+		 * // lectures = this.repository.findManyLecturesByCourseId(curso.getId());
+		 * // for (final Lecture lecture : lectures) {
+		 * // param = String.format("lectureId=%d", lecture.getId());
+		 * // super.checkLinkExists("Sign in");
+		 * // super.request("/lecturer/lecture-course/create" + param);
+		 * // super.checkPanicExists();
+		 * //
+		 * // super.signIn("administrator", "administrator");
+		 * // super.request("/lecturer/lecture-course/create" + param);
+		 * // super.checkPanicExists();
+		 * // super.signOut();
+		 * // }
+		 * // }
+		 */
+
+		// No pueda añadir una lecture a un curso en el que ya está dentro 
+
+		//		Collection<Lecture> lectures;
+		//		final Collection<Course> cursos;
+		//		String param;
+		//
+		//		lectures = this.repository.findManyLecturesByLecturerUsername("user-account-lecturer1");
+		//		cursos = this.repository.findManyCoursesByLecturerUsername("user-account-lecturer1");
+		//
+		//		for (final Lecture leccion : lectures)
+		//			if (leccion.isDraftMode()) {
+		//				param = String.format("id=%d", leccion.getId());
+		//				super.checkLinkExists("Sign in");
+		//				super.request("/lecturer/lecture-course/create?lectureId=83");
+		//				super.checkPanicExists();
+		//
+		//				super.signIn("administrator", "administrator");
+		//				super.request("/lecturer/lecture/create");
+		//				super.checkPanicExists();
+		//				super.signOut();
+		//
+		//			}
+	}
+
+	@Test
+	public void test302Hacking() {
+
+		// No pueda añadir una lecture a un curso alguien que tenga el rol de Lecturer que creó la lecture
+
+		Collection<Lecture> lectures;
+		String param;
+		lectures = this.repository.findManyLecturesByLecturerUsername("user-account-lecturer1");
+
+		super.signIn("lecturer2", "lecturer2");
+
+		for (final Lecture lecture : lectures) {
+			param = String.format("lectureId=%d", lecture.getId());
+			super.request("/lecturer/lecture-course/create" + param);
+			super.checkPanicExists();
+		}
+	}
 }
--- src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseTestRepository.java
+++ src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseTestRepository.java
@@ -6,8 +6,10 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 
 import acme.entities.courses.Course;
+import acme.entities.lectureCourses.LectureCourse;
 import acme.entities.lectures.Lecture;
 import acme.framework.repositories.AbstractRepository;
+import acme.roles.Lecturer;
 
 public interface LecturerLectureCourseTestRepository extends AbstractRepository {
 
@@ -19,4 +21,25 @@ public interface LecturerLectureCourseTestRepository extends AbstractRepository 
 
 	@Query("SELECT i FROM Lecture i WHERE i.title = :title")
 	Lecture findLectureByTitle(String title);
+
+	@Query("select lc from LectureCourse lc where lc.id = :id")
+	LectureCourse findOneLectureCourseById(int id);
+
+	@Query("select l from Lecturer l where l.id = :id")
+	Lecturer findOneLecturerById(int id);
+
+	@Query("select c from Course c where c.lecturer = :lecturer")
+	Collection<Course> findManyCoursesByLecturer(Lecturer lecturer);
+
+	@Query("select c from Course c where c.id = :id")
+	Course findOneCourseById(int id);
+
+	@Query("select l from Lecture l where l.id = :id")
+	Lecture findOneLectureById(int id);
+
+	@Query("select l from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.id = :masterId")
+	Collection<Lecture> findManyLecturesByCourseId(int masterId);
+
+	@Query("select lc from LectureCourse lc where lc.lecture = :lecture and lc.course = :course")
+	LectureCourse findOneLectureCourseByLectureAndCourse(Lecture lecture, Course course);
 }
