5d346f982ba0b4d723639bcb2615e343e5cf92f5
meriglmar <meriglmar@alum.us.es>
2023-05-13 10:25:16 +0200

    Task 151: In progress

---
+++ src/test/java/acme/testing/company/practicum/CompanyPracticumCreateTest.java
@@ -0,0 +1,89 @@
+
+package acme.testing.company.practicum;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.testing.TestHarness;
+
+public class CompanyPracticumCreateTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected CompanyPracticumTestRepository repository;
+
+	// Test methods -----------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/company/practicum/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int practicumIndex, final String course, final String practicumCode, final String title, final String abstract$, final String goals, final String estimatedTotalTime) {
+
+		super.signIn("company1", "company1");
+
+		super.clickOnMenu("Company", "Create practicum");
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("code", practicumCode);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abstract$", abstract$);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("estimatedTotalTime", estimatedTotalTime);
+		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Company", "List of practicums");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(practicumIndex, 0, practicumCode);
+
+		super.clickOnListingRecord(practicumIndex);
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", practicumCode);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstract$", abstract$);
+		super.checkInputBoxHasValue("estimatedTotalTime", estimatedTotalTime);
+		super.checkInputBoxHasValue("course", course);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/company/practicum/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int practicumIndex, final String course, final String practicumCode, final String title, final String abstract$, final String goals, final String estimatedTotalTime) {
+
+		super.signIn("company1", "company1");
+
+		super.clickOnMenu("Company", "Create practicum");
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("code", practicumCode);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abstract$", abstract$);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("estimatedTotalTime", estimatedTotalTime);
+		super.clickOnSubmit("Create");
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		super.checkLinkExists("Sign in");
+		super.request("/company/practicum/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator", "administrator");
+		super.request("/company/practicum/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("auditor1", "auditor1");
+		super.request("/company/practicum/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
+}
