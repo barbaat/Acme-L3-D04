04a848d2ca93f705d4b6cc8c2b4a6e29073ea733
meriglmar <meriglmar@alum.us.es>
2023-05-13 11:21:03 +0200

    Task 151: Company practicum delete test DONE

--- src/test/java/acme/testing/company/practicum/CompanyPracticumDeleteTest.java
+++ src/test/java/acme/testing/company/practicum/CompanyPracticumDeleteTest.java
@@ -1,6 +1,103 @@
+
 package acme.testing.company.practicum;
 
+import java.util.Collection;
 
-public class CompanyPracticumDeleteTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.practicums.Practicum;
+import acme.testing.TestHarness;
+
+public class CompanyPracticumDeleteTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected CompanyPracticumTestRepository repository;
+
+	// Test methods ------------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/company/practicum/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int practicumIndex, final String course, final String practicumCode, final String title, final String abstract$, final String goals, final String estimatedTotalTime) {
+
+		super.signIn("company1", "company1");
+
+		super.clickOnMenu("Company", "List of practicums");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(practicumIndex, 0, practicumCode);
+		super.clickOnListingRecord(practicumIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", practicumCode);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstract$", abstract$);
+		super.checkInputBoxHasValue("goals", goals);
+		super.checkInputBoxHasValue("estimatedTotalTime", estimatedTotalTime);
+
+		final Practicum practicum = this.repository.findPracticumByCode(practicumCode);
+		final String param = String.format("id=%d", practicum.getId());
+		super.clickOnSubmit("Delete");
+		super.checkNotErrorsExist();
+
+		super.request("/company/practicum/delete", param);
+		super.checkPanicExists();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/company/practicum/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int practicumIndex, final String course, final String practicumCode, final String title, final String abstract$, final String goals, final String estimatedTotalTime) {
+
+		super.signIn("company1", "company1");
+
+		super.clickOnMenu("Company", "List of practicums");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(practicumIndex, 0, practicumCode);
+		super.clickOnListingRecord(practicumIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", practicumCode);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstract$", abstract$);
+		super.checkInputBoxHasValue("goals", goals);
+		super.checkInputBoxHasValue("estimatedTotalTime", estimatedTotalTime);
+
+		super.checkNotButtonExists("Delete");
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		Collection<Practicum> practicums;
+		String param;
+
+		practicums = this.repository.findManyPracticumsByCompanyUsername("user-account-company1");
+		for (final Practicum practicum : practicums) {
+			param = String.format("id=%d", practicum.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/company/practicum/delete", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator", "administrator");
+			super.request("/company/practicum/delete", param);
+			super.checkPanicExists();
+			super.signOut();
+		}
+	}
 
 }
