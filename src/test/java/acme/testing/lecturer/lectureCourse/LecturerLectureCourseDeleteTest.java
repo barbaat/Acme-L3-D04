89e2969e8174f823a09aebdf3e6ac1a04a41fb45
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-05-18 15:05:29 +0200

    Fixed minor bugs

--- src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseDeleteTest.java
+++ src/test/java/acme/testing/lecturer/lectureCourse/LecturerLectureCourseDeleteTest.java
@@ -132,11 +132,11 @@ public class LecturerLectureCourseDeleteTest extends TestHarness {
 			param = String.format("lectureId=%d", lecture.getId());
 
 			super.checkLinkExists("Sign in");
-			super.request("/lecturer/lecture-course/create", param);
+			super.request("/lecturer/lecture-course/delete", param);
 			super.checkPanicExists();
 
 			super.signIn("administrator", "administrator");
-			super.request("/lecturer/lecture-course/create", param);
+			super.request("/lecturer/lecture-course/delete", param);
 			super.checkPanicExists();
 			super.signOut();
 		}
@@ -155,7 +155,7 @@ public class LecturerLectureCourseDeleteTest extends TestHarness {
 
 		for (final Lecture lecture : lectures) {
 			param = String.format("lectureId=%d", lecture.getId());
-			super.request("/lecturer/lecture-course/create", param);
+			super.request("/lecturer/lecture-course/delete", param);
 			super.checkPanicExists();
 		}
 	}
